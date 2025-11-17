f7d157dee94486e9477f8edff481d738e16e5cac
CrabGopher <7549475+vedhavyas@users.noreply.github.com>
2025-11-17 17:02:42 +0530

    Initial commit

---
+++ substrate-p2p/src/lib.rs
@@ -0,0 +1,4 @@
+#![deny(unused_crate_dependencies)]
+#![forbid(unsafe_code)]
+pub mod discovery;
+pub mod notifications;
---
+++ substrate-p2p/src/notifications.rs
@@ -0,0 +1,4 @@
+pub mod behavior;
+pub mod handler;
+mod handshake;
+mod messages;
